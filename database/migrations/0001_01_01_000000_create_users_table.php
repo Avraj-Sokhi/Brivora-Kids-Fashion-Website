b2a47b8de8bbc45eecee8357665253b76c7892a6
misannno <misanomamuli@gmail.com>
2025-11-25 14:03:52 +0000

    feature: Login Implementation

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->enum('role', ['customer', 'admin'])->default('customer');
+    $table->boolean('must_change_password')->default(true);
+    
             $table->rememberToken();
             $table->timestamps();
         });
